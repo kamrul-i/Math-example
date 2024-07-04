710c56116ff54731d71ecdd144927fbd915f5889
offic <offic@DESKTOP-GLF4UNU>
2024-07-04 13:21:10 +0600

    ধারাবাহিক বিজোড় সংখ্যার যোগফল

--- src/main/java/math_example/Sum_of_Digits.java
+++ src/main/java/math_example/Sum_of_Digits.java
@@ -3,26 +3,26 @@ import java.util.Scanner;
 public class Sum_of_Digits {
 
     public static void main(String[] args) {
-
         try (Scanner input = new Scanner(System.in)) {
 
-            // input by Even number print--------------------
-            int sum = 0;
-            int m, n;
-            System.out.print("Enter initial/first number : ");
-            m = input.nextInt();
+            // input by Odd number print------------
+            int num = 0;
+            int a, b;
+            System.out.print("Enter first number : ");
+            a = input.nextInt();
 
-            System.out.print("Enter final/last number : ");
-            n = input.nextInt();
+            System.out.print("Enter last number : ");
+            b = input.nextInt();
 
-            for (int i = m; i <= n; i++) {
-                if (i % 2 == 0) {
-                    sum = sum + i;
+            for (int i = a; i <= b; i++) {
+
+                if (i % 2 != 0) {
+                    num = num + i;
                     System.out.println(i);
                 }
             }
-            System.out.println("The sum of even number's is : " + sum);
-            // input by Even number print--------------------
+            System.out.print("Odd number is : " + num);
+            // input by Odd number print---------------
         }
     }
 }
