16856bfee5c7010f0586a896e0a5258156f98a12
offic <offic@DESKTOP-GLF4UNU>
2024-07-04 15:21:57 +0600

    গুণের নামতা

--- src/main/java/math_example/Multiplication_of_Digit.java
+++ src/main/java/math_example/Multiplication_of_Digit.java
@@ -5,18 +5,15 @@ public class Multiplication_of_Digit {
     public static void main(String[] args) {
         try (Scanner input = new Scanner(System.in)) {
 
-            double p, multiplication = 1;
-            System.out.print("Enter the last number : ");
-            p = input.nextDouble();
+            //namota magic.............................................
+            int num;
+            System.out.print("Enter any number : ");
+            num = input.nextInt();
 
-            for (double i = 1.5; i <= p; i++) {
-                System.out.print(i + " ");
-                multiplication = multiplication * i;
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(num + " x " + i + " = " + num * i);
             }
-            System.out.printf("\n");
-            System.out.print(p + " এর মধ্যে দশমিক সংখ্যাগুলোর গুণফল = "+multiplication);
-            //System.out.printf("%.2f", multiplication);
+            //---------------------------------------------------------
         }
-        //series_1.5 * 2.5 * 3.5 * .................*p
     }
 }
