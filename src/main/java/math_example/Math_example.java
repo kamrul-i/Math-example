31f9ada6e3cbd3a8154b5bfcafdee6f3808e1f42
offic <offic@DESKTOP-GLF4UNU>
2024-07-04 12:35:15 +0600

    Introduction to Math_Example.

---
+++ src/main/java/math_example/Math_example.java
@@ -0,0 +1,8 @@
+package math_example;
+
+public class Math_example {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome! Introduction to \"Math_Example.\"");
+    }
+}
