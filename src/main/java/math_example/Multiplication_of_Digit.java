de145543de24e9b337229e8e3609716bc6b788d8
offic <offic@DESKTOP-GLF4UNU>
2024-07-04 15:36:35 +0600

    গুণের নামতা লেখার চমৎকার পদ্ধতি

--- src/main/java/math_example/Multiplication_of_Digit.java
+++ src/main/java/math_example/Multiplication_of_Digit.java
@@ -3,17 +3,23 @@ import java.util.Scanner;
 public class Multiplication_of_Digit {
 
     public static void main(String[] args) {
+        //namota magic.............................................
         try (Scanner input = new Scanner(System.in)) {
+            
+            int m, n;
+            System.out.print("Enter initial/first number : ");
+            m = input.nextInt();
 
-            //namota magic.............................................
-            int num;
-            System.out.print("Enter any number : ");
-            num = input.nextInt();
+            System.out.print("Enter final/last number : ");
+            n = input.nextInt();
 
-            for (int i = 1; i <= 10; i++) {
-                System.out.println(num + " x " + i + " = " + num * i);
+            for (int i = m; i <= n; i++) {
+                for (int j = 1; j <= 10; j++) {
+                    System.out.println(i + " x " + j + " = " + i * j);
+                }
+                System.out.println("\n");
             }
-            //---------------------------------------------------------
         }
+        //---------------------------------------------------------
     }
 }
