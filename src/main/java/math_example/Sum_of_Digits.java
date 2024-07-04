ae0aaacf4b4b49d5491fb84be8e1ba953b7199d7
offic <offic@DESKTOP-GLF4UNU>
2024-07-04 13:16:23 +0600

    ধারাবাহিক জোড় সংখ্যার যোগফল

--- src/main/java/math_example/Sum_of_Digits.java
+++ src/main/java/math_example/Sum_of_Digits.java
@@ -5,27 +5,24 @@ public class Sum_of_Digits {
     public static void main(String[] args) {
 
         try (Scanner input = new Scanner(System.in)) {
+
+            // input by Even number print--------------------
             int sum = 0;
             int m, n;
-            System.out.print("Enter first number : ");
+            System.out.print("Enter initial/first number : ");
             m = input.nextInt();
 
-            System.out.print("Enter last number : ");
+            System.out.print("Enter final/last number : ");
             n = input.nextInt();
 
             for (int i = m; i <= n; i++) {
-                sum = sum + i;
-                System.out.println(i + " "); //যে number গুলো যোগ করবেন সেগুলো result এর মধ্যে দেখতে চাইলে, এ line use করুন।।
-
+                if (i % 2 == 0) {
+                    sum = sum + i;
+                    System.out.println(i);
+                }
             }
-            System.out.print("The sum is = " + sum);
+            System.out.println("The sum of even number's is : " + sum);
+            // input by Even number print--------------------
         }
-        /*
-        'double' or 'float' use করলে decimal result দেখতে পাবে।।
-        ’int’ use করলে পূর্ণ সংখ্যার result দেখতে পাবে।।
-        নিজের সুবিধামতো ক্রমিক সংখ্যার যোগফল বের করতে এ function ব্যবহার করুন।।
-        এ function-এ যতগুলো number ইচ্ছা যোগ করতে পারবেন।।
-         */
     }
-
 }
